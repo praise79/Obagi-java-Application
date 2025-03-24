e51fa5f88bd17535f6079451c2e1c72d7ac6f012
praise79 <praisehaggai13@gmail.com>
2025-03-24 15:59:50 +0100

    Operators

---
+++ chapter 2/Operators.java
@@ -0,0 +1,13 @@
+public class Operators{
+	public static void main(String[] args){
+		//Assignment operator(=)
+		
+		int num1 = 13;
+		int num2 = 5;
+		
+		//Arithmetic operator(+,*,-,/,%)
+		int sum = num1 + num2;
+		
+		System.out.printf("The sum of the numbers is %d%n",sum);
+	}
+}
